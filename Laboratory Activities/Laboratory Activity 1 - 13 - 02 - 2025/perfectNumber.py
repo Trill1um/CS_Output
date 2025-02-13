47076639c772f1fe8c37ffd0424882b3eeb052fd
Trill1um <97826350+Trill1um@users.noreply.github.com>
2025-02-13 11:37:57 +0800

    Add files via upload

---
+++ Laboratory Activities/Laboratory Activity 1 - 13 - 02 - 2025/perfectNumber.py
@@ -0,0 +1,9 @@
+num=int(input("Enter number: "))
+divisors=[]
+for i in range(1,num):
+    if num%i==0:
+        divisors.append(i)
+if sum(divisors)==num:
+    print(f"{num} is a perfect number.")
+else:
+    print(f"{num} is not a perfect number.")
